e408be70f82050fef44254ff13c8d37c9d129039
Luis Eduardo Rosales Prieto <nye12r@gmail.com>
2019-04-27 22:57:41 -0500

    Commit inicial

---
+++ miPrimeraApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  miPrimeraApp
+//
+//  Created by Luis Eduardo Rosales Prieto on 3/26/19.
+//  Copyright © 2019 Luis Eduardo Rosales Prieto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet var boton: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text="LUIS EDUARDO ROSALES PRIETO"
+        print("Soy el Primer Print")
+        print("Soy el segundo Print")
+        print("Soy el tercero Print")
+
+        
+
+    }
+
+
+}
+
